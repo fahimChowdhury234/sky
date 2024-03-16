93e5c1d570f972402a739bf5209dc356a079e4a7
fahimChowdhury234 <fahimmuntacir234@gmail.com>
2024-03-16 15:39:50 +0600

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand': '#352F44',
+        'gray': '#858585',
+        'dark': '#0F0F0F',
+        'secondary': '#a749ff'
+      }
+    },
+  },
+  plugins: [],
+}
